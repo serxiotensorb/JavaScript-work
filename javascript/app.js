0dc119a51f4e447aadd7a0ca195b2a55f99e2985
serxiotensorb <serxioserxio99@gmail.com>
2025-07-26 15:53:01 +0400

    JS

---
+++ javascript/app.js
@@ -0,0 +1,41 @@
+console.log("Hello, World!");
+console.log("This is a simple JavaScript file.");
+console.log("Make sure to check the console for messages.");
+a= 5
+console.log(typeof a);
+b = "Hello";
+console.log(typeof b);
+c = a + " " + b;
+console.log(typeof c);
+console.log(c);
+d = "Salam, " + "user" + "!";
+console.log(d);
+console.log(typeof d);
+let e = 10;
+let f = 20;
+console.log(e + f);
+console.log(e - f);
+console.log(e * f);
+console.log(e / f);
+console.log(e % f);
+console.log(e ** f);
+console.log(e > f);
+console.log(e < f);
+
+
+const city = "Baku";
+const country = "Azerbaijan";
+console.log(`City: ${city}, Country: ${country}`);
+console.log("City: " + city + ", Country: " + country);
+
+
+
+let n = 99
+let m = 100;
+console.log(n - m)
+
+let s = "Russia";
+let o = "USA";
+let v = "and"
+let l =s + " " + v + " " + o;
+console.log(l);
